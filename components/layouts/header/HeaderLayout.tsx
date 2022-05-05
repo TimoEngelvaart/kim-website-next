7b6ee1871cac0d1754dcac0396a9b131e2627594
Timo <timoengelvaart@live.nl>
2022-05-05 13:39:51 +0200

    feat: make header read route

--- components/layouts/header/HeaderLayout.tsx
+++ components/layouts/header/HeaderLayout.tsx
@@ -1,13 +1,37 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 import styles from './HeaderLayout.module.css';
-
 export interface IHeaderLayout {}
 
 const HeaderLayout: React.FC<IHeaderLayout> = () => {
+  const router = useRouter();
+  const [navBg, setNavBg] = React.useState('mainNav');
+  React.useEffect(() => {
+    switch (window.location.pathname) {
+      case '/':
+        setNavBg('mainNav');
+        break;
+      case '/kim':
+        setNavBg('styles.mainNav');
+        break;
+      case '/projecten':
+        setNavBg('styles.mainNav');
+        break;
+      case '/contact':
+        setNavBg('styles.mainNav');
+        break;
+      case '/posters':
+        setNavBg('styles.mainNav');
+        break;
+      default:
+        setNavBg('mainNav');
+    }
+  }, [router]);
+
   return (
     <nav>
-      <div className={styles.mainNav}>
+      <div className={navBg}>
         <Link href="/">
           <a className={styles.iconLogoIcon2} />
         </Link>
